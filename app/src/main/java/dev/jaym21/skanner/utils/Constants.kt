4321adcabba7892183cb633ff2cd071edcfddc9b
jaym21 <jaynmistry21@gmail.com>
2021-11-21 19:50:19 +0530

    implementing finding largest contours function

--- app/src/main/java/dev/jaym21/skanner/utils/Constants.kt
+++ app/src/main/java/dev/jaym21/skanner/utils/Constants.kt
@@ -4,11 +4,21 @@ class Constants {
     companion object {
         const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
         const val PHOTO_EXTENSION = ".jpg"
+        const val RATIO_4_3_VALUE = 4.0 / 3.0
+        const val RATIO_16_9_VALUE = 16.0 / 9.0
         const val CAMERA_REQUEST_CODE = 1001
         const val READ_EXTERNAL_STORAGE_REQUEST_CODE = 1002
         const val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 1003
         const val IMAGE_ANALYSIS_SCALE_WIDTH = 400
-        const val RATIO_4_3_VALUE = 4.0 / 3.0
-        const val RATIO_16_9_VALUE = 16.0 / 9.0
+        const val BLURRING_KERNEL_SIZE = 5.0
+        const val NORMALIZATION_MIN_VALUE = 0.0
+        const val NORMALIZATION_MAX_VALUE = 255.0
+        const val ANGLES_NUMBER = 4
+        const val EPSILON_CONSTANT = 0.02
+        const val CLOSE_KERNEL_SIZE = 10.0
+        const val CANNY_THRESHOLD_LOW = 75.0
+        const val CANNY_THRESHOLD_HIGH = 200.0
+        const val CUTOFF_THRESHOLD = 155.0
+        const val TRUNCATE_THRESHOLD = 150.0
     }
 }
--- app/src/main/java/dev/jaym21/skanner/utils/OpenCVUtils.kt
+++ app/src/main/java/dev/jaym21/skanner/utils/OpenCVUtils.kt
@@ -1,13 +1,37 @@
 package dev.jaym21.skanner.utils
 
 import dev.jaym21.skanner.model.Quadrilateral
-import org.opencv.core.Mat
+import org.opencv.core.*
+import org.opencv.imgproc.Imgproc
 
 class OpenCVUtils {
     companion object {
 
         fun detectLargestQuadrilateral(src: Mat): Quadrilateral? {
             val destination = Mat()
+            Imgproc.blur(src, src, Size(Constants.BLURRING_KERNEL_SIZE, Constants.BLURRING_KERNEL_SIZE))
+
+            Core.normalize(src, src, Constants.NORMALIZATION_MIN_VALUE, Constants.NORMALIZATION_MAX_VALUE, Core.NORM_MINMAX)
+
+            Imgproc.threshold(src, src, Constants.TRUNCATE_THRESHOLD, Constants.NORMALIZATION_MAX_VALUE, Imgproc.THRESH_TRUNC)
+            Core.normalize(src, src, Constants.NORMALIZATION_MIN_VALUE, Constants.NORMALIZATION_MAX_VALUE, Core.NORM_MINMAX)
+
+            Imgproc.Canny(src, destination, Constants.CANNY_THRESHOLD_HIGH, Constants.CANNY_THRESHOLD_LOW)
+
+            Imgproc.threshold(destination, destination, Constants.CUTOFF_THRESHOLD, Constants.NORMALIZATION_MAX_VALUE, Imgproc.THRESH_TOZERO)
+
+            Imgproc.morphologyEx(
+                destination, destination, Imgproc.MORPH_CLOSE,
+                Mat(Size(Constants.CLOSE_KERNEL_SIZE, Constants.CLOSE_KERNEL_SIZE), CvType.CV_8UC1, Scalar(Constants.NORMALIZATION_MAX_VALUE)),
+                Point(-1.0, -1.0), 1
+            )
+
+            val largestContour: List<MatOfPoint>? = findLargestContours(destination)
+        }
+
+        private fun findLargestContours(inputMat: Mat): List<MatOfPoint>? {
+            val mHierarchy = Mat()
+            val mContourList: List<MatOfPoint> = ArrayList()
         }
     }
 }
