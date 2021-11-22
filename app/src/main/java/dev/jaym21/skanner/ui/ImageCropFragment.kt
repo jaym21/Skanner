e5ce890a7d7767c071ab3d69fe3ae01362ed481f
jaym21 <jaynmistry21@gmail.com>
2021-11-22 18:13:25 +0530

    removed action bar

--- app/src/main/java/dev/jaym21/skanner/ui/ImageCropFragment.kt
+++ app/src/main/java/dev/jaym21/skanner/ui/ImageCropFragment.kt
@@ -1,7 +1,9 @@
 package dev.jaym21.skanner.ui
 
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +19,7 @@ class ImageCropFragment : Fragment() {
     private var binding: FragmentImageCropBinding? = null
     private lateinit var navController: NavController
     private lateinit var takenImageUri: Uri
+    private var selectedImage: Bitmap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,7 +39,9 @@ class ImageCropFragment : Fragment() {
         //getting uri of image taken from argument
         takenImageUri = arguments?.getString("savedUri")!!.toUri()
 
+        selectedImage = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, takenImageUri)
 
+        binding?.ivTakenImage?.setImageBitmap(selectedImage)
     }
 
     override fun onDestroy() {
--- app/src/main/java/dev/jaym21/skanner/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/skanner/ui/MainActivity.kt
@@ -2,13 +2,17 @@ package dev.jaym21.skanner.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import dev.jaym21.skanner.R
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        System.loadLibrary("opencv")
+        if (OpenCVLoader.initDebug()) {
+            Log.d("TAGYOYO", "OpenCV loaded")
+        }
     }
 }
