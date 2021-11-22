4e651e54e0ae1c6fdc4b89f15e89c067a3987ab4
jaym21 <jaynmistry21@gmail.com>
2021-11-22 22:32:49 +0530

    implemented extension function for bitmap to scale taken image to required height and width

--- app/src/main/java/dev/jaym21/skanner/extensions/BitmapExtensions.kt
+++ app/src/main/java/dev/jaym21/skanner/extensions/BitmapExtensions.kt
@@ -1,5 +1,15 @@
 package dev.jaym21.skanner.extensions
 
 import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.RectF
 
-internal fun Bitmap.scaledBitmap(width: Int, )
+internal fun Bitmap.scaledBitmap(width: Int, height: Int): Bitmap {
+    val matrix = Matrix()
+    matrix.setRectToRect(
+        RectF(0f, 0f, this.width.toFloat(), this.height.toFloat()),
+        RectF(0f, 0f, width.toFloat(), height.toFloat()),
+        Matrix.ScaleToFit.CENTER
+    )
+    return Bitmap.createBitmap(this, 0, 0, this.width, this.height, matrix, true)
+}
