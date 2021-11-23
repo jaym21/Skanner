8236f8d8a1ba730525ebd5ecafbe3f671bdb8f5c
jaym21 <jaynmistry21@gmail.com>
2021-11-23 18:49:11 +0530

    added image processing fragment

---
+++ app/src/main/java/dev/jaym21/skanner/ui/ImageProcessingFragment.kt
@@ -0,0 +1,35 @@
+package dev.jaym21.skanner.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.skanner.R
+import dev.jaym21.skanner.databinding.FragmentImageProcessingBinding
+
+
+class ImageProcessingFragment : Fragment() {
+
+    private var binding: FragmentImageProcessingBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentImageProcessingBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
