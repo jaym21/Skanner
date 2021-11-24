6676cdf34ad66fd766165255686f36be70f4fd2e
jaym21 <jaynmistry21@gmail.com>
2021-11-24 18:06:38 +0530

    implementing applying grayscale to image

--- app/src/main/java/dev/jaym21/skanner/ui/ImageProcessingFragment.kt
+++ app/src/main/java/dev/jaym21/skanner/ui/ImageProcessingFragment.kt
@@ -7,10 +7,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import dev.jaym21.skanner.R
 import dev.jaym21.skanner.databinding.FragmentImageProcessingBinding
+import dev.jaym21.skanner.extensions.rotate
+import dev.jaym21.skanner.utils.Constants
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.io.File
 
 
@@ -20,6 +27,8 @@ class ImageProcessingFragment : Fragment() {
     private lateinit var navController: NavController
     private var croppedImageFile: File? = null
     private var croppedImageBitmap: Bitmap? = null
+    private var tempBitmap: Bitmap? = null
+    private var isGrayScaleApplied: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,14 +56,39 @@ class ImageProcessingFragment : Fragment() {
             navController.popBackStack(R.id.allDocumentsFragment, true)
         }
 
+        tempBitmap = croppedImageBitmap
+
         binding?.llRotate?.setOnClickListener {
             rotateImage()
+        }
+
+        binding?.llGrayScale?.setOnClickListener {
+            applyGrayScale()
         }
     }
 
     private fun rotateImage() {
-        if (croppedImageBitmap!= null) {
+        binding?.progressBar?.visibility = View.VISIBLE
+        if (tempBitmap != null) {
+            tempBitmap = tempBitmap?.rotate(Constants.ANGLE_OF_ROTATION)
+            binding?.ivCroppedImage?.setImageBitmap(tempBitmap)
+            binding?.progressBar?.visibility = View.GONE
+        } else {
+            Toast.makeText(requireContext(), "No image found, try again!", Toast.LENGTH_SHORT)
+                .show()
+            navController.popBackStack(R.id.allDocumentsFragment, true)
+            binding?.progressBar?.visibility = View.GONE
+        }
+    }
 
+    private fun applyGrayScale() {
+        binding?.progressBar?.visibility = View.VISIBLE
+        if (tempBitmap != null) {
+            if (!isGrayScaleApplied) {
+                binding?.ivGrayscaleIcon?.setColorFilter(ContextCompat.getColor(requireContext(), R.color.blue_500))
+                binding?.tvGrayScale?.setTextColor(ContextCompat.getColor(requireContext(), R.color.blue_500))
+
+            }
         }
     }
 
--- app/src/main/java/dev/jaym21/skanner/utils/Constants.kt
+++ app/src/main/java/dev/jaym21/skanner/utils/Constants.kt
@@ -4,12 +4,9 @@ class Constants {
     companion object {
         const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
         const val PHOTO_EXTENSION = ".jpg"
-        const val RATIO_4_3_VALUE = 4.0 / 3.0
-        const val RATIO_16_9_VALUE = 16.0 / 9.0
         const val CAMERA_REQUEST_CODE = 1001
         const val READ_EXTERNAL_STORAGE_REQUEST_CODE = 1002
         const val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 1003
-        const val IMAGE_ANALYSIS_SCALE_WIDTH = 400
         const val BLURRING_KERNEL_SIZE = 5.0
         const val NORMALIZATION_MIN_VALUE = 0.0
         const val NORMALIZATION_MAX_VALUE = 255.0
@@ -20,12 +17,10 @@ class Constants {
         const val CANNY_THRESHOLD_HIGH = 200.0
         const val CUTOFF_THRESHOLD = 155.0
         const val TRUNCATE_THRESHOLD = 150.0
-        const val TIME_POST_PICTURE = 1500L
         const val DOWNSCALE_IMAGE_SIZE = 600.0
         const val FIRST_MAX_CONTOURS = 10
         const val SMALLEST_ANGLE_COS = 0.172 //80 degrees
-        const val CLEAR_SHAPE_DELAY_IN_MILLIS = 600L
-        const val POINTER_ANIMATION_DURATION = 300L
+        const val ANGLE_OF_ROTATION = 90
         const val TRANSFORMED_IMAGE_NAME = "transformedImage"
     }
 }
