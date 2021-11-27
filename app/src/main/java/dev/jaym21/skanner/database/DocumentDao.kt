8ec1244aeea6a4922c754572c1c59b583986d8fa
jaym21 <jaynmistry21@gmail.com>
2021-11-27 11:47:14 +0530

    added delete function in dao

--- app/src/main/java/dev/jaym21/skanner/database/DocumentDao.kt
+++ app/src/main/java/dev/jaym21/skanner/database/DocumentDao.kt
@@ -1,6 +1,7 @@
 package dev.jaym21.skanner.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import dev.jaym21.skanner.models.Document
@@ -11,5 +12,6 @@ interface DocumentDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertDocument(document: Document)
 
-
+    @Delete
+    suspend fun deleteDocument(document: Document)
 }
