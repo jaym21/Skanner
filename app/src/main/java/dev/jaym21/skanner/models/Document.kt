9b806633fcdb537d13e27494a2ff8a56cede7146
jaym21 <jaynmistry21@gmail.com>
2021-11-27 11:44:16 +0530

    added document model

--- app/src/main/java/dev/jaym21/skanner/models/Document.kt
+++ app/src/main/java/dev/jaym21/skanner/models/Document.kt
@@ -1,9 +1,13 @@
 package dev.jaym21.skanner.models
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "document_table")
 data class Document(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
-
+    val name: String,
+    val path: String,
+    val pageCount: Int
 )
