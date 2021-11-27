5dd8db015ec59ae4456e1846d5a3251c1a2e73f0
jaym21 <jaynmistry21@gmail.com>
2021-11-27 11:46:41 +0530

    added insert function in dao

--- app/src/main/java/dev/jaym21/skanner/database/DocumentDao.kt
+++ app/src/main/java/dev/jaym21/skanner/database/DocumentDao.kt
@@ -1,8 +1,15 @@
 package dev.jaym21.skanner.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import dev.jaym21.skanner.models.Document
 
 @Dao
 interface DocumentDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertDocument(document: Document)
+
 
 }
