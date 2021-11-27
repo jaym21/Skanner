f42d192357d322e3657e2f923fb4a053cd5b5eae
jaym21 <jaynmistry21@gmail.com>
2021-11-27 11:45:30 +0530

    adding document dao

---
+++ app/src/main/java/dev/jaym21/skanner/database/DocumentDao.kt
@@ -0,0 +1,8 @@
+package dev.jaym21.skanner.database
+
+import androidx.room.Dao
+
+@Dao
+interface DocumentDao {
+
+}
--- app/src/main/java/dev/jaym21/skanner/database/DocumentDatabase.kt
+++ app/src/main/java/dev/jaym21/skanner/database/DocumentDatabase.kt
@@ -2,7 +2,10 @@ package dev.jaym21.skanner.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import dev.jaym21.skanner.models.Document
 
-@Database(entities = [])
+@Database(entities = [Document::class], version = 1, exportSchema = false)
 abstract class DocumentDatabase: RoomDatabase() {
+
+
 }
