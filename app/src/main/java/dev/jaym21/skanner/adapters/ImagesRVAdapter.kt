771ec4869ed6d2c5f2e2a44443039af275682d03
jaym21 <jaynmistry21@gmail.com>
2021-11-28 10:14:55 +0530

    implementing document images recycler view adapter

---
+++ app/src/main/java/dev/jaym21/skanner/adapters/ImagesRVAdapter.kt
@@ -0,0 +1,12 @@
+package dev.jaym21.skanner.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class ImagesRVAdapter: ListAdapter<ImagesRVAdapter.ImagesViewHolder>() {
+
+    inner class ImagesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
