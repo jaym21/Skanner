e7b584b5ff050cabcfb63dd704bf065fcf1aff9b
jaym21 <jaynmistry21@gmail.com>
2021-12-06 22:36:22 +0530

    changing checking storage permission to splash activity

--- app/src/main/java/dev/jaym21/skanner/ui/SplashActivity.kt
+++ app/src/main/java/dev/jaym21/skanner/ui/SplashActivity.kt
@@ -1,18 +1,90 @@
 package dev.jaym21.skanner.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavGraph
+import dev.jaym21.skanner.R
+import dev.jaym21.skanner.utils.Constants
 
 class SplashActivity: AppCompatActivity() {
+
+    private var isStoragePermissionGranted = false
+    private var readPermissionGranted = false
+    private var writePermissionGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if()
+        updateOrRequestPermissions()
+//
+//        if(isStoragePermissionGranted) {
+//
+//        } else {
+//            val intent = Intent(this, PermissionNotGrantedActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+    }
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+    private fun updateOrRequestPermissions() {
+        val isReadPermissionAvailable = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        val isWritePermissionAvailable = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        val minSDK29 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+
+        readPermissionGranted = isReadPermissionAvailable
+        writePermissionGranted = isWritePermissionAvailable || minSDK29
+
+
+//        if (readPermissionGranted && writePermissionGranted) {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+
+        if (!writePermissionGranted) {
+            ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), Constants.WRITE_EXTERNAL_STORAGE_REQUEST_CODE)
+        }
+
+        if (!readPermissionGranted) {
+            ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), Constants.READ_EXTERNAL_STORAGE_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            Constants.WRITE_EXTERNAL_STORAGE_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    val intent = Intent(this, PermissionNotGrantedActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            Constants.READ_EXTERNAL_STORAGE_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    val intent = Intent(this, PermissionNotGrantedActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
     }
 }
