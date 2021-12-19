b1905c10696920dadb6bee52da6d7bc36672575e
jaym21 <jaynmistry21@gmail.com>
2021-12-19 10:34:51 +0530

    added navController to text extract fragment

--- app/src/main/java/dev/jaym21/skanner/ui/TextExtractFragment.kt
+++ app/src/main/java/dev/jaym21/skanner/ui/TextExtractFragment.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.Camera
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import dev.jaym21.skanner.R
 import dev.jaym21.skanner.databinding.FragmentTextExtractBinding
 
@@ -12,6 +18,10 @@ import dev.jaym21.skanner.databinding.FragmentTextExtractBinding
 class TextExtractFragment : Fragment() {
 
     private var binding: FragmentTextExtractBinding? = null
+    private lateinit var navController: NavController
+    private var preview: Preview? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +35,8 @@ class TextExtractFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //initializing navController
+        navController = Navigation.findNavController(view)
     }
 
     override fun onDestroy() {
